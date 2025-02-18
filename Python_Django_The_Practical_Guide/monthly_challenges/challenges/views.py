6f9e4e6a86c80117bb3f877d4992f3884d2445aa
ronnyfph <ronnyfph@gmail.com>
2025-02-18 13:05:53 +0100

    Finished Section 3 from the Django Project

--- Python_Django_The_Practical_Guide/monthly_challenges/challenges/views.py
+++ Python_Django_The_Practical_Guide/monthly_challenges/challenges/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
@@ -15,15 +16,33 @@ monthly_challenges = {
     "december": "Spend quality time with family and friends!"
 }
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("monthly-challenge", args=[month])
+        list_items += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+
+
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    redirect_month = months[month - 1]
+    redirect_path = reverse("monthly-challenge", args=[redirect_month])  # /challenge/january
+    return HttpResponseRedirect(redirect_path)
 
 
 # noinspection PyBroadException
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("Sorry, that month is not available yet.")
-
-    return HttpResponse(challenge_text)
+        return HttpResponseNotFound("<h1>Sorry, that month is not supported!</h1>")
